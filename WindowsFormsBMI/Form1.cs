0cc955ce4381fc0b120c2aa7ebd76b49d98ccbc3
a5780446 <anyi11070406@gmail.com>
2021-06-15 17:36:22 +0800

    加入專案檔案。

---
+++ WindowsFormsBMI/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsBMI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        float w;
+        float h;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text ="您的BMI為:"+ (w / (h * h)).ToString("#0.01");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (float.TryParse(textBox1.Text, out h)) ;
+            h = h * 0.01F;
+            
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (float.TryParse(textBox2.Text, out w)) ;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
